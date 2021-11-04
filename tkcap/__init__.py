61017edb64ca01c6b3a5c7a8abad5054f944cf33
ghanteyyy <ghanteyyy@gmail.com>
2021-11-04 08:53:13 +0545

    Removed unnecessary codes

--- tkcap/__init__.py
+++ tkcap/__init__.py
@@ -2,40 +2,37 @@
 tkcap
 
 A wrapper for tkinter window for taking its screenshot.
-By ghanteyyy http://github.com/ghanteyyy
+By ghanteyyy https://github.com/ghanteyyy
 MIT License
 
 Usage:
     import tkcap
 
-    cap = tkcap.CAP(master)    # master is an instance of tkinter
+    cap = tkcap.CAP(master)     # master is an instance of tkinter
     cap.capture(FileName)       # Capture and Save the screenshot of the tkiner window
 
     # If you want the x_pos, y_pos, width and height of the tkinter window.
     region = cap.get_region()
 
     # If you want to bind the key so that everytime you press that key
-    # captures the screenshot. Here I have binded "Control g":
-    # Set 'overwrite' parameter to True, to overwrite the file having same name
+    # captures the screenshot. Here I have binded to "Control g"
     master.bind('<Control-g>', lambda: cap.capture(FileName))
 '''
 
 __all__ = ['CAP']
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'ghanteyyy'
 
 import os
 import sys
-import winsound
 import subprocess
 import collections
 import tkinter as tk
 import tkinter.tix as tix
-
 from . import exceptions
 
 if sys.version_info.major < 3:
-    raise exceptions.TkCapException('tkcap supports Python 3+')
+    raise exceptions.TkCapException('tkcap supports Python 3+ only')
 
 try:
     import pyautogui
@@ -51,14 +48,13 @@ class CAP:
     def __init__(self, master):
         if isinstance(master, (tk.Tk, tk.Toplevel, tix.Tk, tix.Toplevel)):
             self.master = master
-            self.wav_path = os.path.join(os.path.dirname(__file__), 'data', 'shutter.wav')
             self.VALID_EXTENSION = ['tif', 'tiff', 'jpg', 'png', 'jpeg', 'jpe', 'jfif', 'bmp', 'dib', 'gif']
 
         else:
             raise exceptions.TkCapException('master parameter was expected to be the instance of tkinter')
 
     def get_region(self):
-        '''Gets x-coordinate, y-coordinate, width and height of the tkinter window'''
+        '''Get x-coordinate, y-coordinate, width and height of the tkinter window'''
 
         self.master.update()
 
@@ -67,8 +63,10 @@ class CAP:
 
         return Region(x_pos, y_pos, width, height)
 
-    def capture(self, imageFilename, overwrite=False):
-        '''Capture and save screenshot of the tkinter window'''
+    def capture(self, imageFilename, overwrite=False, show=False):
+        '''Capture and save screenshot of the tkinter window
+           Set 'overwrite' parameter to True, to overwrite the file having same name
+           Set 'show' parameter to True, to open the image file in file explorer'''
 
         path = os.path.abspath(os.path.join('.', imageFilename))
         head, tail = os.path.split(path)
@@ -78,12 +76,6 @@ class CAP:
             raise exceptions.PathNotFoundError(f'The system cannot find path: {head}')
 
         elif extension not in self.VALID_EXTENSION:
-            if '.' in tail:
-                extension = '.' + extension
-
-            else:  # If extension is not provided by the user.
-                extension = ''
-
             raise exceptions.ExtensionError(f'unknown file extension: {extension}')
 
         elif os.path.exists(path) and overwrite is False:
@@ -92,8 +84,7 @@ class CAP:
         if overwrite:
             os.remove(path)
 
-        winsound.PlaySound(self.wav_path, winsound.SND_ASYNC)
-
         pyautogui.screenshot(path, region=self.get_region())
 
-        self.master.after(1100, lambda: subprocess.run([os.path.join(os.getenv('WINDIR'), 'explorer.exe'), '/select,', os.path.normpath(path)]))
+        if show:
+            self.master.after(1100, lambda: subprocess.run([os.path.join(os.getenv('WINDIR'), 'explorer.exe'), '/select,', os.path.normpath(path)]))
